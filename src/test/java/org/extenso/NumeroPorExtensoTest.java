13f180f6eabc82c04cb80519bd73217adf47f3ac
ocaffe <leandrocaffe@gmail.com>
2016-02-16 14:45:18 -0200

    Suporte a decimais e qualificadores (reais/quilos...)

--- src/test/java/org/extenso/NumeroPorExtensoTest.java
+++ src/test/java/org/extenso/NumeroPorExtensoTest.java
@@ -1,6 +1,7 @@
 package org.extenso;
 
 import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
 import org.junit.Test;
 
@@ -8,126 +9,167 @@ public class NumeroPorExtensoTest {
 
     @Test
     public void representacaoUnidade() {
-        assertEquals("um", extenso(1));
-        assertEquals("nove", extenso(9));
+        assertEquals("um", NumeroPorExtenso.get(1));
+        assertEquals("nove", NumeroPorExtenso.get(9));
     } 
     
     @Test
     public void representacaoDezena() {
-        assertEquals("dez", extenso(10));
-        assertEquals("noventa", extenso(90));
+        assertEquals("dez", NumeroPorExtenso.get(10));
+        assertEquals("noventa", NumeroPorExtenso.get(90));
     }
     
     @Test
     public void representacaoCentena() {
-        assertEquals("cem", extenso(100));
-        assertEquals("novecentos", extenso(900));
+        assertEquals("cem", NumeroPorExtenso.get(100));
+        assertEquals("novecentos", NumeroPorExtenso.get(900));
     }
 
     @Test
     public void conectorCasas_dezenaMaisUnidade() {
-        assertEquals("vinte e dois", extenso(22));
+        assertEquals("vinte e dois", NumeroPorExtenso.get(22));
     }
     
     @Test
     public void conectorCasas_centenaMaisDezena() {
-        assertEquals("duzentos e vinte", extenso(220));
+        assertEquals("duzentos e vinte", NumeroPorExtenso.get(220));
     }
     
     @Test
     public void conectorCasas_centenaMaisDezenaMaisUnidade() {
-        assertEquals("duzentos e vinte e dois", extenso(222));
+        assertEquals("duzentos e vinte e dois", NumeroPorExtenso.get(222));
     }
     
     @Test
     public void representacaoSegundaClasse() {
-        assertEquals("dois mil", extenso(2000));
+        assertEquals("dois mil", NumeroPorExtenso.get(2000));
     }
     
     @Test
     public void representacaoTerceiraClasse() {
-        assertEquals("um milhão", extenso(1000000));
+        assertEquals("um milhão", NumeroPorExtenso.get(1000000));
     }
     
     @Test
     public void representacaoQuartaClasse() {
-        assertEquals("um bilhão", extenso(1000000000));
+        assertEquals("um bilhão", NumeroPorExtenso.get(1000000000));
     }
     
     @Test
     public void representacaoQuintaClasse() {
-        assertEquals("um trilhão", extenso(1000000000000l));
+        assertEquals("um trilhão", NumeroPorExtenso.get(1000000000000l));
     }
     
     @Test
     public void representacaoSextaClasse() {
-        assertEquals("um quatrilhão", extenso(1000000000000000l));
+        assertEquals("um quatrilhão", NumeroPorExtenso.get(1000000000000000l));
     }
     
     @Test
     public void conectorClasses() {
-        assertEquals("um milhão, duzentos e trinta e quatro mil, quinhentos e sessenta e sete", extenso(1234567));
+        assertEquals("um milhão, duzentos e trinta e quatro mil, quinhentos e sessenta e sete", 
+            NumeroPorExtenso.get(1234567));
     }
     
     @Test
     public void onzeADezenoveNaoTemConector() {
-        assertEquals("onze", extenso(11));
-        assertEquals("dezenove", extenso(19));
+        assertEquals("onze", NumeroPorExtenso.get(11));
+        assertEquals("dezenove", NumeroPorExtenso.get(19));
     }
     
     @Test
     public void cem_cento() {
-        assertEquals("cem", extenso(100));
-        assertEquals("cento e dez", extenso(110));
-        assertEquals("cento e um", extenso(101));
+        assertEquals("cem", NumeroPorExtenso.get(100));
+        assertEquals("cento e dez", NumeroPorExtenso.get(110));
+        assertEquals("cento e um", NumeroPorExtenso.get(101));
     }
     
     @Test
     public void unidadeSegundaClasseComRepresentacao() {
-        assertEquals("dois mil", extenso(2000));
+        assertEquals("dois mil", NumeroPorExtenso.get(2000));
     }
     
     @Test
     public void unidadeSegundaClasseSemRepresentacao() {
-        assertEquals("mil", extenso(1000));
+        assertEquals("mil", NumeroPorExtenso.get(1000));
     }
     
     @Test
     public void classeZeradaNaoTemRepresentacao() {
-        assertEquals("um trilhão, cento e vinte e um", extenso(1000000000121l));
+        assertEquals("um trilhão, cento e vinte e um", NumeroPorExtenso.get(1000000000121l));
     }
     
     @Test
     public void segundaClasseNaoFlexionaNumero() {
-        assertEquals("mil", extenso(1000));
-        assertEquals("dois mil", extenso(2000));
+        assertEquals("mil", NumeroPorExtenso.get(1000));
+        assertEquals("dois mil", NumeroPorExtenso.get(2000));
     }
     
     @Test
     public void acimaDaSegundaClasseFlexionaNumero() {
-        assertEquals("um milhão", extenso(1000000));
-        assertEquals("dois milhões", extenso(2000000));
+        assertEquals("um milhão", NumeroPorExtenso.get(1000000));
+        assertEquals("dois milhões", NumeroPorExtenso.get(2000000));
         
-        assertEquals("um bilhão", extenso(1000000000));
-        assertEquals("dois bilhões", extenso(2000000000));
+        assertEquals("um bilhão", NumeroPorExtenso.get(1000000000));
+        assertEquals("dois bilhões", NumeroPorExtenso.get(2000000000));
         
-        assertEquals("um trilhão", extenso(1000000000000l));
-        assertEquals("dois trilhões", extenso(2000000000000l));
+        assertEquals("um trilhão", NumeroPorExtenso.get(1000000000000l));
+        assertEquals("dois trilhões", NumeroPorExtenso.get(2000000000000l));
     }
     
     @Test
     public void ultimaClasseComValorConectaComESeCentenaEhZero() {
-        assertEquals("mil e dez", extenso(1010));
-        assertEquals("um milhão e dez mil", extenso(1010000));
+        assertEquals("mil e dez", NumeroPorExtenso.get(1010));
+        assertEquals("um milhão e dez mil", NumeroPorExtenso.get(1010000));
     }
     
     @Test
     public void ultimaClasseComValorConectaComESeCentenaEhRazos() {
-        assertEquals("mil e cem", extenso(1100));
-        assertEquals("um milhão e cem mil", extenso(1100000));
+        assertEquals("mil e cem", NumeroPorExtenso.get(1100));
+        assertEquals("um milhão e cem mil", NumeroPorExtenso.get(1100000));
     }
     
-    private String extenso(long valor) {
-        return NumeroPorExtenso.get(valor);
+    @Test
+    public void qualificador() {
+        
+        assertEquals("um dia", NumeroPorExtenso.get(1, "dia", "dias"));
+        assertEquals("dois dias", NumeroPorExtenso.get(2, "dia", "dias"));
+    }
+    
+    @Test
+    public void decimalDuasCasas() {
+        
+        assertEquals("um centavo", 
+            NumeroPorExtenso.get(new BigDecimal("0.01"), "real", "reais", "centavo", "centavos"));
+        
+        assertEquals("dez centavos", 
+            NumeroPorExtenso.get(new BigDecimal("0.10"), "real", "reais", "centavo", "centavos"));
+    }
+    
+    @Test
+    public void decimalTresCasas() {
+        
+        assertEquals("um grama", 
+            NumeroPorExtenso.get(new BigDecimal("0.001"), "quilo", "quilos", "grama", "gramas"));
+        
+        assertEquals("cem gramas", 
+            NumeroPorExtenso.get(new BigDecimal("0.100"), "quilo", "quilos", "grama", "gramas"));
+    }
+    
+    @Test
+    public void decimalZerado() {
+
+        assertEquals("um real", 
+            NumeroPorExtenso.get(new BigDecimal("1.00"), "real", "reais", "centavo", "centavos"));
+    }
+    
+    @Test
+    public void inteiroMaisDecimal() {
+        
+        assertEquals("um real e um centavo", 
+            NumeroPorExtenso.get(new BigDecimal("1.01"), "real", "reais", "centavo", "centavos"));
+        
+        assertEquals("dois reais e vinte centavos", 
+            NumeroPorExtenso.get(new BigDecimal("2.20"), "real", "reais", "centavo", "centavos"));
     }
 }
