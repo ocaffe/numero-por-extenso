805a5087a8a23ed068db20dc86722eb7f7002595
Caffé <leandrocaffe@gmail.com>
2016-02-19 11:06:54 -0200

    Encoding e suporte a valores em Reais também no aplicativo (Main.java).

--- src/main/java/org/extenso/Main.java
+++ src/main/java/org/extenso/Main.java
@@ -1,5 +1,6 @@
 package org.extenso;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Main {
@@ -11,21 +12,25 @@ public class Main {
             String entry = "";
 
             do {
-                System.out.print("Escreva um número: ");
-                entry = in.nextLine();
-
-                long numero = 0;
-
+                System.out.print("Escreva um número inteiro ou um valor em reais: ");
+                entry = in.nextLine().replaceAll("\\.", "");
+                
                 try {
+                    
+                    if (entry.contains(",")) {
+                        
+                        System.out.println(
+                            NumeroPorExtenso.get(new BigDecimal(entry.replace(",", ".")), "real", "reais", "centavo", "centavos"));
 
-                    numero = Long.parseLong(entry.replaceAll("\\.", ""));
+                    } else {
 
+                        System.out.println(NumeroPorExtenso.get(Long.parseLong(entry)));
+                    }
                 } catch (NumberFormatException e) {
                     System.out.println("Número inválido.");
                     continue;
                 }
 
-                System.out.println("O seu número por extenso: " + NumeroPorExtenso.get(numero));
 
             } while (!"".equals(entry));
 
